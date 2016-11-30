1e597c37b7df8784fb2581744c6ae3eef1358140
Hendrik Heneke <hendrik@pixelwunder.net>
2016-11-30 19:33:30 +0100

    Set OutputWriter if OutputInterface is provided as an argument.

--- src/main/php/Console/DoctrineKernel.php
+++ src/main/php/Console/DoctrineKernel.php
@@ -8,9 +8,16 @@ use HHIT\Doctrine\Console\DoctrineClient;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Console\Kernel;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class DoctrineKernel extends Kernel
 {
+
+    /**
+     * @var OutputInterface
+     */
+    private $output;
 
     protected $commands = [];
 
@@ -27,10 +34,20 @@ class DoctrineKernel extends Kernel
         $this->commands[] = $commandClass;
     }
 
+    public function handle($input, $output = null)
+    {
+        if ($output == null) {
+            $this->output = new ConsoleOutput();
+        } else {
+            $this->output = $output;
+        }
+        return parent::handle($input, $output);
+    }
+
     protected function getArtisan()
     {
         $artisan = parent::getArtisan();
-        foreach (DoctrineClient::createHelperSet($this->app->make(EntityManagerInterface::class), $this->app->make(Configuration::class)) as $key => $helper) {
+        foreach (DoctrineClient::createHelperSet($this->app->make(EntityManagerInterface::class), $this->app->make(Configuration::class, [$this->output])) as $key => $helper) {
             $artisan->getHelperSet()->set($helper, $key);
         }
         return $artisan;
--- src/main/php/Migrations/Providers/MigrationsProvider.php
+++ src/main/php/Migrations/Providers/MigrationsProvider.php
@@ -3,6 +3,7 @@
 namespace HHIT\Doctrine\Illuminate\Migrations\Providers;
 
 use Doctrine\DBAL\Migrations\Configuration\Configuration;
+use Doctrine\DBAL\Migrations\OutputWriter;
 use Doctrine\ORM\EntityManagerInterface;
 use HHIT\Doctrine\Illuminate\Migrations\IlluminateMigrationsConfigurationSource;
 use HHIT\Doctrine\Migrations\Contracts\MigrationsConfigurationFactory;
@@ -11,7 +12,10 @@ use HHIT\Doctrine\Migrations\Contracts\MigrationsHandler;
 use HHIT\Doctrine\Migrations\MigrationsDefaultConfigurationFactory;
 use HHIT\Doctrine\Migrations\MigrationsDefaultHandler;
 use HHIT\Doctrine\ORM\Contracts\EntityManagerConfigurationSource;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Console\Output\Output;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class MigrationsProvider extends ServiceProvider
 {
@@ -49,8 +53,23 @@ class MigrationsProvider extends ServiceProvider
                 $this->app->make(EntityManagerInterface::class)
             );
         });
-        $this->app->singleton(Configuration::class, function () {
-            return $this->app->make(MigrationsConfigurationFactory::class)->createConfiguration();
+        $this->app->singleton(Configuration::class, function (Application $app, $args) {
+            $configuration = $this->app->make(MigrationsConfigurationFactory::class)->createConfiguration();
+
+            $outputInterface = null;
+            if ($args != null && is_array($args)) {
+                foreach ($args as $arg) {
+                    if ($arg instanceof OutputInterface) {
+                        $outputInterface = $arg;
+                    }
+                }
+            }
+            if ($outputInterface != null) {
+                $configuration->setOutputWriter(new OutputWriter(function ($message) use ($outputInterface) {
+                    $outputInterface->writeln($message);
+                }));
+            }
+            return $configuration;
         });
     }
 
